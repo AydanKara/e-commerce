54b97533a7cc222d9e590ac0b39d4166178ed0d1
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-01-15 22:37:33 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -96,6 +96,17 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
-
-
 //! slider end
+
+//! add product to localStorage
+async function getData() {
+  const photos = await fetch("../e-commerce/js/data.json");
+  const data = await photos.json();
+
+  data ? localStorage.setItem("products", JSON.stringify(data)) : [];
+}
+
+getData();
+
+const products = localStorage.getItem("products");
+console.log(JSON.parse(products));
