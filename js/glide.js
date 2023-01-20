5d6b457d501db933d03912b61fb0e7ce46e48f2e
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-01-20 21:24:51 +0100

    updates

--- js/glide.js
+++ js/glide.js
@@ -2,6 +2,7 @@ const productsContainer = document.getElementById("product-list");
 
 export function product1() {
     const config = {
+      type: "carousel",
       perView: 4,
       gap: 20,
       /*   autoplay: 3000, */
@@ -22,6 +23,7 @@ export function product1() {
   }
   
   const config2 = {
+    type: "carousel",
     perView: 4,
     gap: 20,
     /*   autoplay: 3000, */
@@ -40,3 +42,22 @@ export function product1() {
   };
   
   productsContainer && new Glide(".product-carousel2", config2).mount();
+
+  const config3 = {
+    perView: 5,
+    gap: 20,
+  
+    breakpoints: {
+      992: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+      576: {
+        perView: 1,
+      },
+    },
+  };
+  
+new Glide(".product-thumb", config3).mount();
