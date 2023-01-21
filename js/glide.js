011cc7331e55267b74c05b55ff26cec10fed1797
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-01-21 13:33:06 +0100

    single product thumbnails

--- js/glide.js
+++ js/glide.js
@@ -2,33 +2,11 @@ const productsContainer = document.getElementById("product-list");
 const productsGallery = document.getElementById("gallery-thumbs");
 
 export function product1() {
-    const config = {
-      type: "carousel",
-      perView: 4,
-      gap: 20,
-      /*   autoplay: 3000, */
-      breakpoints: {
-        992: {
-          perView: 3,
-        },
-        768: {
-          perView: 2,
-        },
-        576: {
-          perView: 1,
-        },
-      },
-    };
-  
-    productsContainer && new Glide(".product-carousel", config).mount();
-  }
-  
-  const config2 = {
+  const config = {
     type: "carousel",
     perView: 4,
     gap: 20,
     /*   autoplay: 3000, */
-    
     breakpoints: {
       992: {
         perView: 3,
@@ -41,12 +19,16 @@ export function product1() {
       },
     },
   };
-  
-productsContainer && new Glide(".product-carousel2", config2).mount();
 
-  
-const config3 = {
-  perView: 5,
+  productsContainer && new Glide(".product-carousel", config).mount();
+}
+
+const config2 = {
+  type: "carousel",
+  perView: 4,
+  gap: 20,
+  /*   autoplay: 3000, */
+
   breakpoints: {
     992: {
       perView: 3,
@@ -60,4 +42,22 @@ const config3 = {
   },
 };
 
-productsGallery && new Glide(".product-thumb", config3).mount();
+productsContainer && new Glide(".product-carousel2", config2).mount();
+
+export function singleThumbs() {
+  const config3 = {
+    perView: 5,
+    breakpoints: {
+      992: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+      576: {
+        perView: 1,
+      },
+    },
+  };
+  new Glide(".product-thumb", config3).mount();
+}
