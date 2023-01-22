514ec8221cd154aba9136e5d392cf6425aa29869
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-01-22 07:08:39 +0100

    modal dialog & search.js update

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ import searchFunc from "./search.js";
 //! add product to localStorage start
 (async function () {
   const photos = await fetch("../e-commerce/js/data.json");
-  const data = await photos.json();
+  const products = await photos.json();
 
-  data ? localStorage.setItem("products", JSON.stringify(data)) : [];
+  products ? localStorage.setItem("products", JSON.stringify(products)) : [];
   productsFunc();
-  searchFunc(data);
+  searchFunc(products);
 })();
 //! add product to localStorage end
 
@@ -20,3 +20,24 @@ cartItems.innerHTML = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart")).length
   : "0";
 //! add cartItems to localStorage end
+
+//! modal dialog start
+const modalDialogDOM = document.querySelector(".modal-dialog");
+const modalContentDOM = document.querySelector(".modal-dialog .modal-content");
+const btnCloseDialog = document.querySelector(".modal-dialog .modal-close");
+
+btnCloseDialog.addEventListener("click", function () {
+  modalDialogDOM.classList.remove("show");
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.composedPath().includes(modalContentDOM)) {
+    modalDialogDOM.classList.remove("show");
+  }
+});
+
+setTimeout(() => {
+  modalDialogDOM.classList.add("show");
+}, 3000);
+//! modal dialog end
+
--- js/search.js
+++ js/search.js
@@ -12,14 +12,13 @@ function productRoute() {
       });
     });
   }
-
-function searchFunc(data) {
-  console.log(data);
-  const searchWrapperDOM = document.querySelector(".search-results .results");
-  let result = "";
-  data.forEach((item) => {
-    result += `
-        <a href="#" class="result-item" data-id=${item.id}>
+  
+  function searchFunc(products) {
+    const searchWrapperDOM = document.querySelector(".search-results .results");
+    let result = "";
+    products.forEach((item) => {
+      result += `
+      <a href="#" class="result-item" data-id=${item.id}>
             <img src="${item.img.singleImage}" class="search-thump" alt=""/>
             <div class="search-info">
                 <h4>${item.name}</h4>
@@ -28,9 +27,62 @@ function searchFunc(data) {
             </div>
         </a>
       `;
-  });
-  searchWrapperDOM.innerHTML = result;
-  productRoute();
-}
-
-export default searchFunc;
+    });
+    searchWrapperDOM.innerHTML = result;
+    productRoute();
+  
+    const searchInputDOM = document.querySelector(".search-form input");
+    let value = "";
+    let filtered = [];
+    searchInputDOM.addEventListener("input", (e) => {
+      value = e.target.value;
+      value = value.trim().toLowerCase();
+      filtered = products.filter((item) =>
+        item.name.trim().toLowerCase().includes(value)
+      );
+      let result = "";
+      if (filtered.length > 1) {
+        searchWrapperDOM.style.gridTemplateColumns = "1fr 1fr";
+        filtered.forEach((item) => {
+          result += `
+            <a href="#" class="result-item" data-id=${item.id}>
+                <img src="${item.img.singleImage}" class="search-thump" alt=""/>
+                <div class="search-info">
+                    <h4>${item.name}</h4>
+                    <span class="search-sku">SKU: PD0016</span>
+                    <span class="search-price">$${item.price.newPrice.toFixed(2)}</span>
+                </div>
+            </a>
+          `;
+        });
+        searchWrapperDOM.innerHTML = result;
+      } else if (filtered.length < 2) {
+        searchWrapperDOM.style.gridTemplateColumns = "1fr";
+        if (filtered == 0) {
+          searchWrapperDOM.innerHTML = `
+          <a href="#" class="result-item" style="justify-content: center">
+          😔The product you were looking for was not found😔
+          </a>
+          `;
+        } else {
+          filtered.forEach((item) => {
+            result += `
+            <a href="#" class="result-item" data-id=${item.id}>
+                <img src="${item.img.singleImage}" class="search-thump" alt=""/>
+                <div class="search-info">
+                    <h4>${item.name}</h4>
+                    <span class="search-sku">SKU: PD0016</span>
+                    <span class="search-price">$${item.price.newPrice.toFixed(2)}</span>
+                </div>
+            </a>
+            `;
+          });
+          searchWrapperDOM.innerHTML = result;
+        }
+      }
+  
+      productRoute();
+    });
+  }
+  
+  export default searchFunc;
